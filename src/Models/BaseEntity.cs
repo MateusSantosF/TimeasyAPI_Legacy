fca3cb1f468a400b87744867b8676ad015246cf9
MateusSantosF <mateusferreira.dev@gmail.com>
2023-08-11 20:41:23 -0300

    Add BaseEntity model

---
+++ src/Models/BaseEntity.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TimeasyAPI.src.Models
+{
+    public abstract class BaseEntity
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        public bool Active { get; set; }    
+        
+        protected BaseEntity()
+        {
+            Id = Guid.NewGuid();
+        }
+    }
+}
