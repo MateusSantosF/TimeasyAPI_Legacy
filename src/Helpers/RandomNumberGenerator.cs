65ee8599c575f9159692ee2fef50efc3630393ec
MateusSantosF <mateusferreira.dev@gmail.com>
2023-08-11 20:40:01 -0300

    Add RandomNumberGenerator helper

---
+++ src/Helpers/RandomNumberGenerator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace TimeasyAPI.src.Helpers
+{
+    public static class RandomNumberGenerator
+    {
+        private static Random random = new Random();
+        public static string GenerateRandomNumber(int length)
+        {
+            const string characters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+            char[] result = new char[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                result[i] = characters[random.Next(characters.Length)];
+            }
+
+            return new string(result);
+        }
+    }
+}
