404310604effde4f97e6e0dad3ca10d525303cfe
MateusSantosF <mateusferreira.dev@gmail.com>
2023-08-12 15:28:44 -0300

    Add PaginationExtension method

---
+++ src/Repositories/Interfaces/IBaseRepository.cs
@@ -0,0 +1,17 @@
+﻿using TimeasyAPI.src.Models;
+
+namespace TimeasyAPI.src.Repositories.Interfaces
+{
+    public interface IBaseRepository<T> where T : BaseEntity
+    {
+
+        public Task<T> Create(T entity);
+
+        public Task<T> Update(T entity);
+
+        public void Delete(T entity);   
+
+        public Task<T> GetById(Guid id);
+        
+    }
+}
