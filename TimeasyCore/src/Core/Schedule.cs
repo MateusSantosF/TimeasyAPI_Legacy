2f6ffaeb0957e9fac61ccc5bb23f8bf04ac13fa4
MateusSantosF <mateusferreira.dev@gmail.com>
2023-10-10 23:29:45 -0300

    feat: add mockGenerator

---
+++ TimeasyCore/src/Core/Schedule.cs
@@ -0,0 +1,29 @@
+﻿namespace TimeasyCore.src.Core
+{
+    public class Schedule
+    {
+        public Dictionary<DayOfWeek, List<Class>> ClassesByDay { get; }
+
+        public Schedule()
+        {
+            ClassesByDay = new Dictionary<DayOfWeek, List<Class>>();
+            InitializeDaysOfWeek();
+        }
+        private void InitializeDaysOfWeek()
+        {
+            foreach (DayOfWeek day in Enum.GetValues(typeof(DayOfWeek)))
+            {
+                ClassesByDay[day] = new List<Class>();
+            }
+        }
+
+        public void AddClass(DayOfWeek day, Class classItem)
+        {
+            if (ClassesByDay.ContainsKey(day))
+            {
+                ClassesByDay[day].Add(classItem);
+            }
+        }
+    }
+
+}
